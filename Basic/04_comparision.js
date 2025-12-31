5b68149279c55b6144163e04af1c9039b2abf164
Aryan Jaiswal <aj80529754@gmail.com>
2025-12-31 13:39:18 +0000

    commit config

---
+++ Basic/04_comparision.js
@@ -0,0 +1,12 @@
+// console.log(2>1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);  |
+// console.log(null == 0); | => Avoid this type of conversion
+// console.log(null >= 0); |
+
+//=== Triple equalto check strickly datatype and value
+console.log("2"=== 2);
+console.log(2 === 2);
