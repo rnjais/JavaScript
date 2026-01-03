fe467a49ddb30257d303df3bc5116b272db88296
Aryan Jaiswal <aj80529754@gmail.com>
2026-01-03 09:31:57 +0000

    commit

--- Basic/09_array.js
+++ Basic/09_array.js
@@ -12,4 +12,27 @@ console.log(anotherArray);
 anotherArray.pop(10);//Pop is used to remove element from array
 console.log(anotherArray);
 
+console.log(anotherArray.includes(9));//false
+console.log(anotherArray.indexOf(4));//1
+
+const modifyarry = anotherArray.join();//Convert number Array in String
+console.log(modifyarry);
+console.log(typeof(modifyarry));
+
+//+++++++++++++++++Slice++++++++++++++
+
+console.log("Original Array",anotherArray);
+const myn1 = anotherArray.slice(1,3);
+console.log("Slice",myn1);
+console.log("After Slice Original Array",anotherArray);// Nothing changed in Array after Slice
+
+//++++++++++++++++Splice++++++++++++++++++++++
+console.log("Original Array",anotherArray);
+console.log("After Splice ",anotherArray.splice(3,2));
+console.log("Original Array",anotherArray);
+
+
+
+
+
 
