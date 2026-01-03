c39f8b272f05cc7b7cc08b164988e72e7f7d17ac
Aryan Jaiswal <aj80529754@gmail.com>
2026-01-03 07:47:27 +0000

    nums and math

---
+++ Basic/07_numsandmath.js
@@ -0,0 +1,27 @@
+//*********************Number*******************
+const score = 400
+console. log(score);
+
+const balance = new Number(100)
+console. log(balance);
+
+console. log(balance. toString().length);
+console. log(balance. toString(). length);
+
+ const hundreds = 1000000
+ console. log(hundreds. toLocaleString('en-IN' ));
+
+console. log(balance. toFixed(1));
+
+const otherNumber = 123.8966
+
+console. log(otherNumber. toPrecision(4));
+
+//********************Math********************
+console.log ( Math);
+console.log(Math. abs(-4));//4
+console.log(Math. round (4.6));//5
+console.log(Math.ceil(4.2));//5
+console.log(Math. floor(4.9));//4
+console.log(Math.min(4, 3, 6,8));//3
+console.log(Math.max(4, 3, 6, 8));//8
